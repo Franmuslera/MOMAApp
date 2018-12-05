70a08366e7d5879783a9ccba2577bca02624d2ea
Unknown <francisco.muslera@gmail.com>
2018-12-05 18:56:11 -0300

    mvc

---
+++ app/src/main/java/com/example/digital/momapp/model/POJO/Paints.java
@@ -0,0 +1,17 @@
+package com.example.digital.momapp.model.POJO;
+
+import java.io.Serializable;
+
+public class Paints implements Serializable{
+
+    private String nombre;
+    private String imagen;
+    private Integer artistId;
+
+    public Paints() {
+
+    }
+
+
+
+}
